d66ac3e718f0b4ca9b96e4bfcc727d3d8b4a8abc
Matheus Henrique Rodrigues de Oliveira da Silva <matheus.silva@MARJAN.CORP>
2023-01-02 17:11:38 -0300

    commit dos popups corretos

--- Js/principal.js
+++ Js/principal.js
@@ -1,2 +1,10 @@
+// const button = document.getElementsByClassName('.popup-button')
+// const popup = document.getElementsByClassName('.popup-wrapper')
 
-
+// button.onclick = function(){
+    
+//     popup.style.display = 'block'
+// };
+// button.addEventListener('click', () => {
+//      popup.style.display = 'block'
+//     });
